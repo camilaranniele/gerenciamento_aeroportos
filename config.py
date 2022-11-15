f9f64cd1b2967df38cdea988f5c2a40bbf1bf4e2
Camila Ranniele <rannielecamsil@gmail.com>
2022-11-15 13:18:18 -0300

    Arquivo config com conexao ao bd e algumas configuracoes

---
+++ config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://empresa_user:123456' \
+                                        '@localhost/aeroportos'
+
+db = SQLAlchemy(app)
+marshmallow = Marshmallow(app)
