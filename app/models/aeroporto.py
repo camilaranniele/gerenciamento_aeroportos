1123ee7052d55b04d214af90b51bd7d8b1dda3d9
Camila Ranniele <rannielecamsil@gmail.com>
2022-11-15 13:17:06 -0300

    model e schema de aeroportos

---
+++ app/models/aeroporto.py
@@ -0,0 +1,29 @@
+from config import db
+
+class AeroportoDataBase(db.Model):
+    __tablename__ = "Aeroporto"
+    id_aeroporto = db.Column(db.Integer, primary_key=True)
+    nome_aeroporto = db.Column(db.String(256), unique=True, nullable=False)
+    codigo_iata = db.Column(db.String(3), unique=True, nullable=False)
+    cidade = db.Column(db.String(256), nullable=False)
+    codigo_pais_iso = db.Column(db.String(2), nullable=False)
+    latitude = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    longitude = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    altitude = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+
+    def __init__(self, nome_aeroporto, codigo_iata, cidade, codigo_pais_iso, latitude, longitude, altitude):
+        self.nome_aeroporto = nome_aeroporto
+        self.codigo_iata = codigo_iata
+        self.cidade = cidade
+        self.codigo_pais_iso = codigo_pais_iso
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __repr__(self):
+        return f"{self.id_aeroporto, self.nome_aeroporto, self.codigo_iata, self.cidade, self.codigo_pais_iso, self.latitude, self.longitude, self.altitude}"
---
+++ app/models/aeroporto_schema.py
@@ -0,0 +1,18 @@
+from marshmallow import fields
+
+from app.models.aeroporto import AeroportoDataBase
+from config import marshmallow, db
+
+class AeroportoDataBaseSchema(marshmallow.SQLAlchemyAutoSchema):
+    class Meta:
+        model = AeroportoDataBase
+        sqla_session = db.session
+
+    id_aeroporto = fields.Number()  # dump_only=True)
+    nome_aeroporto = fields.String(required=True)
+    codigo_iata = fields.String(required=True)
+    cidade = fields.String(required=True)
+    codigo_pais_iso = fields.String(required=True)
+    latitude = fields.Float(required=True)
+    longitude = fields.Float(required=True)
+    altitude = fields.Float(required=True)
