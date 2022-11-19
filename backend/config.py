4862640722ebf7aa930dea196385ecaa3eb4c4e2
mariavitoriav <mvbv.vitoria@gmail.com>
2022-11-19 20:13:14 -0300

    habilitar cors

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/aeroportos'
 
 db = SQLAlchemy(app)
